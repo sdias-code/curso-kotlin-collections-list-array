dd1b09c89162a54d8735a84ef625c8a77062f4b5
sdias-code <silviodias.ms@gmail.com>
2021-03-19 23:57:34 -0400

    Add file TesteList.kt

---
+++ kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -0,0 +1,26 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    val joao = Funcionario("João", 1000.0)
+    val pedro = Funcionario("Pedro", 2000.0)
+    val maria = Funcionario("Maria", 4000.0)
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach { println(it) }
+
+    println("----------------------")
+    println(funcionarios.find { it.nome == "Maria" })
+
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double
+){
+    override fun toString(): String =
+        """
+            Nome: $nome
+            Salário: $salario
+        """.trimIndent()
+    }
