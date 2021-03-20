b43462a439bc2cbc6475fefc947f7fc6b4f111bf
sdias-code <silviodias.ms@gmail.com>
2021-03-19 23:29:14 -0400

    Add file TesteOperacoes.kt Corrigido max e min Depreciados

--- kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -8,8 +8,8 @@ fun main(){
     }
 
     println("-------------------")
-    println("Maior salario: ${salarios.max()}")
-    println("Menor salario: ${salarios.min()}")
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
     println("Media salarial: ${salarios.average()}")
 
     val salariosMaiorQue2500 = salarios.filter { it > 2500 }
