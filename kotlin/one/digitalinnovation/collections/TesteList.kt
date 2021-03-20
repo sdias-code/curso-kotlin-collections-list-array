72341c8498415beffe84bcac453d127f1e2277d5
sdias-code <silviodias.ms@gmail.com>
2021-03-20 00:39:43 -0400

    Add file TesteList.kt usando o método sortedBy e groupBy

--- kotlin/one/digitalinnovation/collections/TesteList.kt
+++ kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main(){
-    val joao = Funcionario("João", 2000.0)
-    val pedro = Funcionario("Pedro", 1000.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 2000.0, "PJ")
+    val pedro = Funcionario("Pedro", 1000.0, "CLT")
+    val maria = Funcionario("Maria", 4000.0, "PJ")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -17,16 +17,23 @@ fun main(){
         .sortedBy { it.salario }
         .forEach { println(it) }
 
+    println("----------------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
 
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
     override fun toString(): String =
         """
             Nome: $nome
             Salário: $salario
+            Tipo: $tipoContratacao
         """.trimIndent()
     }
